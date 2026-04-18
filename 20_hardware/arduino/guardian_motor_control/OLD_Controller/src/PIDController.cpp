a543391f3204890b00bf3aedffc122a192ab54a7
Vedant Patkar <vedantpatkar07@gmail.com>
2026-04-18 02:41:19 -0400

    New Commit

---
+++ 20_hardware/arduino/guardian_motor_control/OLD_Controller/src/PIDController.cpp
@@ -0,0 +1,22 @@
+#include "PIDController.h"
+
+PIDController::PIDController(float p, float i, float d, float dt, float max_i) {
+    Kp = p; Ki = i; Kd = d; 
+    iteration_time = dt; max_integral = max_i;
+    integral = 0.0f; error_prior = 0.0f;
+}
+
+float PIDController::compute(float desired_velocity, float actual_velocity) {
+    float error = desired_velocity - actual_velocity;
+    integral = integral + (error * iteration_time);
+    
+    if (integral > max_integral) integral = max_integral;
+    else if (integral < -max_integral) integral = -max_integral;
+    
+    float derivative = (error - error_prior) / iteration_time;
+    
+    float output = (Kp * error) + (Ki * integral) + (Kd * derivative);
+    error_prior = error;
+    
+    return output; 
+}
